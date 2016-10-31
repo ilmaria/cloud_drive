74b3285a50442100f88b7be8ca792acb1a77053f
Ilmari Autio <ilmari.autio@gmail.com>
2016-10-31 19:53:46 +0200

    Try __before__compile__ for template fix

--- lib/cloud_drive/template.ex
+++ lib/cloud_drive/template.ex
@@ -1,18 +1,22 @@
 defmodule CloudDrive.Template do
 
-  defmacro __using__(_opts) do
+  defmacro __before_compile__(_env) do
     quote do
       require EEx
 
       view_path = "./lib/views/"
       files = File.ls!(view_path)
       
-      Enum.each files, fn file ->
+      Enum.map files, fn file ->
         render_func = file |> String.replace(".", "") |> String.to_atom
-        EEx.function_from_string(
-          :defp, render_func, view_path <> file, [:assigns])
+        EEx.function_from_string(:defp,
+          render_func, view_path <> file, [:assigns])
       end
-      
+    end
+  end
+
+  defmacro __using__(_opts) do
+    quote do
       def render(file, bindings \\ []) do
         render_func = file |> String.replace(".", "") |> String.to_atom
         apply(__MODULE__, render_func, [bindings])
