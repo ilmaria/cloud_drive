7b8aef50e3451ec3e34164b06820421ee80a5d84
Ilmari Autio <ilmari.autio@gmail.com>
2016-11-02 23:29:34 +0200

    Change to vanilla js

--- web/components/app.js
+++ web/components/app.js
@@ -1,59 +1,47 @@
-import xs from 'xstream'
-import {run} from '@cycle/xstream-run'
-import {makeDOMDriver, h2, div} from '@cycle/dom'
-import SearchBar from './search-bar'
-import FileView from './file-view'
+'use strict'
 
-function App(input) {
-  const searchBar = SearchBar(input)
-  const fileData = xs.of({
-    files: [
-      {
-        name: 'kissa.txt',
-        id: 0,
-        tags: [1, 3],
-        modified_time: '2016-11-02'
-      },
-      {
-        name: 'dogs.txt',
-        id: 2,
-        tags: [2],
-        modified_time: '2016-11-02'
-      }
-    ],
-    tags: [
-      {
-        id: 1,
-        name: 'fun'
-      },
-      {
-        id: 2,
-        name: 'cool'
-      },
-      {
-        id: 3,
-        name: 'something'
-      }
-    ]
-  })
+const fileTemplate = document.querySelector('#file-item-template').content
+const data = {
+  files: [
+    {
+      name: 'kissa.txt',
+      id: 0,
+      tags: [1, 3],
+      modified_time: '2016-11-02'
+    },
+    {
+      name: 'dogs.txt',
+      id: 2,
+      tags: [2],
+      modified_time: '2016-11-02'
+    }
+  ],
+  tags: [
+    {
+      id: 1,
+      name: 'fun'
+    },
+    {
+      id: 2,
+      name: 'cool'
+    },
+    {
+      id: 3,
+      name: 'something'
+    }
+  ]
+}
 
-  const fileView = FileView({search: searchBar.value, data: fileData})
+function updateFileList(files) {
+  const fileList = document.querySelector('#search-files')
 
-  const view$ = xs.combine(searchBar.DOM, fileView.DOM)
-    .map(([searchBarDOM, fileViewDOM]) => {
-      div([
-        searchBarDOM,
-        fileViewDOM
-      ])
-    })
-
-  return {
-    DOM: view$
+  for (let file of files) {
+    const fileItem = document.importNode(fileTemplate, true)
+    fileItem.querySelector('li').textContent = file.name
+    fileList.appendChild(fileItem)
   }
+
+  fileList.classList.toggle('display-none', files.length == 0)
 }
 
-const drivers = {
-  DOM: makeDOMDriver('main')
-}
-
-run(App, drivers)
+updateFileList(data.files)
