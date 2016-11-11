36bdb62a8a2423c8b9f72951af6639f85ea0cc4a
Ilmari Autio <ilmari.autio@gmail.com>
2016-11-11 12:19:50 +0200

    Add row selection

--- web/js/app.js
+++ web/js/app.js
@@ -12,6 +12,19 @@ $('input[type="search"]').addEventListener('input', event => {
   showSearchResults(foundFiles, string.length != 0)
 })
 
+const tableRows = $$('tbody tr')
+
+for (const row of tableRows) {
+  row.addEventListener('click', event => {
+    // remove old selected classes
+    for (const i of tableRows) {
+      i.classList.remove('selected')
+    }
+    // mark current row as selected
+    row.classList.add('selected')
+  })
+}
+
 function showSearchResults(files, show) {
   // change header visibilities
   $('#search-results-h').classList.toggle('display-none', !show)
@@ -20,10 +33,10 @@ function showSearchResults(files, show) {
 
   const tableRows = $$('#all-files tbody tr')
 
-  for (let row of tableRows) {
+  for (const row of tableRows) {
     const fileId = Number(row.id.split('-')[2])
     const inFileResults = files.find(x => x.id === fileId)
-    
+
     if (show) {
       row.classList.toggle('display-none', !inFileResults)
     } else {
