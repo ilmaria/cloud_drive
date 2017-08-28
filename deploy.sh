6b0a9ce1ac7b84f9bfc19ffd17d1ddbc8e6ffef0
Ilmari Autio <ilmari.autio@gmail.com>
2017-08-28 21:30:59 +0300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-set -eu
 
-mosh cloud_drive-server
-cd cloud_drive
-
-mix deps.get
-MIX_ENV=prod mix release --env=prod --no-tar
-
+ssh -tt cloud_drive-server <<'ENDSSH'
+(cd cloud_drive &&
+git pull &&
+mix deps.get &&
+MIX_ENV=prod mix release --env=prod --no-tar &&
 _build/prod/rel/cloud_drive/bin/cloud_drive start
+logout)
+ENDSSH
